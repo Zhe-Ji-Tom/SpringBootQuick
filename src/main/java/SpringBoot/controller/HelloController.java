d2a3147e3f4705207bd9798839bf6a58ecf167c1
Tom <404526222@qq.com>
2020-02-09 14:31:44 +0800

    Tom

--- src/main/java/SpringBoot/controller/HelloController.java
+++ src/main/java/SpringBoot/controller/HelloController.java
@@ -1,13 +1,26 @@
-package SpringBoot.controller;
+ package SpringBoot.controller;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	
+	@ResponseBody
 	@RequestMapping("/hello")
 	public String helloController() {
 		return "Hello World Quick!";
+	}
+	
+	@RequestMapping("/success")
+	public String success(Map<String, Object> map) {
+		map.put("hello", "你好");
+		map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu")); 
+		return "success";
 	}
 }
--- src/test/java/SpringBoot/SpringBootQuickApplicationTests.java
+++ src/test/java/SpringBoot/SpringBootQuickApplicationTests.java
@@ -2,6 +2,8 @@ package SpringBoot;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -19,14 +21,20 @@ class SpringBootQuickApplicationTests {
 	@Autowired
 	ApplicationContext ioc;
 	
-	@Test
-	public void testHelloService() {
-		System.out.println(ioc.containsBean("helloService"));
-	}
+	Logger logger = LoggerFactory.getLogger(getClass());
 	
+//	@Test
+//	public void testHelloService() {
+//		System.out.println(ioc.containsBean("helloService"));
+//	}
+//	
 	@Test
 	void contextLoads() {
-		System.out.println(person);
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("error");
 	}
 
 }
