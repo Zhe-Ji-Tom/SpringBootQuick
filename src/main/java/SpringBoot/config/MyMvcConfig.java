e38c8cd13de01dfcaac2feeb64decb6e84908adf
Tom <404526222@qq.com>
2020-02-12 18:24:43 +0800

    Tom

--- src/main/java/SpringBoot/config/MyMvcConfig.java
+++ src/main/java/SpringBoot/config/MyMvcConfig.java
@@ -1,9 +1,13 @@
 package SpringBoot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import SpringBoot.component.MyLocaleResolver;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer{
@@ -11,5 +15,10 @@ public class MyMvcConfig implements WebMvcConfigurer{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
 		registry.addViewController("/index.html").setViewName("index");
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new MyLocaleResolver();
 	}
 }
